2f17df5632085efc9354830b2b9b567530304e97
Michael Platt <mikepl@microsoft.com>
2016-12-07 16:45:43 +0000

    Final Cleanup

--- FunctionFile/Functions.js
+++ FunctionFile/Functions.js
@@ -14,94 +14,23 @@ function statusUpdate(icon, text) {
   });
 }
 
-// Adds text into the body of the item, then reports the results
-// to the info bar.
-function addTextToBody(text, icon, event) {
-  Office.context.mailbox.item.body.setSelectedDataAsync(text, { coercionType: Office.CoercionType.Text }, 
-    function (asyncResult){
-      if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
-        statusUpdate(icon, "\"" + text + "\" inserted successfully.");
-      }
-      else {
-        Office.context.mailbox.item.notificationMessages.addAsync("addTextError", {
-          type: "errorMessage",
-          message: "Failed to insert \"" + text + "\": " + asyncResult.error.message
-        });
-      }
-      event.completed();
-    });
-}
 
-function addDefaultMsgToBody(event) {
-  addTextToBody("Inserted by the Add-in Command Demo add-in.", "blue-icon-16", event);
-}
-
-function addMsg1ToBody(event) {
-  addTextToBody("Hello World!", "red-icon-16", event);
-}
-
-function addMsg2ToBody(event) {
-  addTextToBody("Add-in commands are cool!", "red-icon-16", event);
-}
-
-function addMsg3ToBody(event) {
-  addTextToBody("Visit https://dev.outlook.com today for all of your add-in development needs.", "red-icon-16", event);
-}
 
 // Gets the subject of the item and displays it in the info bar.
-function getSubject(event) {
+function verify(event) {
   var subject = Office.context.mailbox.item.subject;
-
-
-
-
-stampery = new Stampery('5819d1e5-7eb7-4583-d557-f4dfd9bc2d1c', 'beta')
-
-  stampery.on('ready', function() {
-  stampery.receiveMissedProofs();
-  stampery.hash(subject, function(hash) {
-    stampery.stamp(hash);
-  });
-});
-
   
-  Office.context.mailbox.item.notificationMessages.addAsync("subject", {
+  Office.context.mailbox.item.notificationMessages.addAsync("Verify", {
     type: "informationalMessage",
     icon: "blue-icon-16",
-    message: "Email Verified" + hash,
+    message: "Verified: " + "hash",
     persistent: false
   });
   
   event.completed();
 }
 
-// Gets the item class of the item and displays it in the info bar.
-function getItemClass(event) {
-  var itemClass = Office.context.mailbox.item.itemClass;
-  
-  Office.context.mailbox.item.notificationMessages.addAsync("itemClass", {
-    type: "informationalMessage",
-    icon: "red-icon-16",
-    message: "Item Class: " + itemClass,
-    persistent: false
-  });
-  
-  event.completed();
-}
 
-// Gets the date and time when the item was created and displays it in the info bar.
-function getDateTimeCreated(event) {
-  var dateTimeCreated = Office.context.mailbox.item.dateTimeCreated;
-  
-  Office.context.mailbox.item.notificationMessages.addAsync("dateTimeCreated", {
-    type: "informationalMessage",
-    icon: "red-icon-16",
-    message: "Created: " + dateTimeCreated.toLocaleString(),
-    persistent: false
-  });
-  
-  event.completed();
-}
 
 // Gets the ID of the item and displays it in the info bar.
 function getItemID(event) {
